5c87431b72f37eaaa7b6fb331afe6e7bcb39e032
adsinc <sinc86@gmail.com>
2016-04-05 23:36:32 +0300

    tbd

--- src/main/scala/tagservice/service/TagServiceHandler.scala
+++ src/main/scala/tagservice/service/TagServiceHandler.scala
@@ -2,15 +2,43 @@ package tagservice.service
 
 import com.twitter.util.Future
 
+import scala.collection.mutable
+
 class TagServiceHandler extends TagService.FutureIface {
-  override def addTag(record: Record, tag: Tag): Future[Unit] = ???
+  val tags = mutable.Map[Long, Tag]()
+  val records = mutable.Map[Long, Record]()
+  val recordsToTags = mutable.Map[Long, Set[Long]]()
 
-  override def getRecords(tags: Seq[Tag]): Future[Seq[Record]] = ???
+  override def addTag(record: Record, tag: Tag): Future[Unit] = Future {
+    //todo check args ???
+    val tagId = tag.id
+    val recId = record.id
+    if (!records.contains(recId) && tags.contains(tagId)) {
+      //todo validation
+      ???
+    }
+    val currentTags = recordsToTags(recId)
+    if(currentTags.contains(tagId)) {
+      ???
+    }
+    recordsToTags += recId -> (currentTags + tag.id)
+  }
 
-  override def deleteTag(record: Record, tag: Tag): Future[Unit] = ???
+  override def getRecords(tags: Seq[Tag]): Future[Seq[Record]] = Future {
+    val tagIds = tags.map(_.id).toSet
+    (for {
+      (recId, regTagIds) <- recordsToTags
+      tagId <- regTagIds
+      if tagIds.contains(tagId)
+    } yield records(recId)).toSeq
+  }
 
-  override def getTags(record: Record): Future[Seq[Tag]] = {
-    println(s"Request $record")
-    Future.value(Seq(Tag(1, "helloTag")))
+  override def deleteTag(record: Record, tag: Tag): Future[Unit] = Future {
+    recordsToTags += record.id -> (recordsToTags(record.id) - tag.id)
+  }
+
+  override def getTags(record: Record): Future[Seq[Tag]] = Future {
+    val tagIds = recordsToTags(record.id)
+    tags.filterKeys(tagIds.contains).values.toSeq
   }
 }
