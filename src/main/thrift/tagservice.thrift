8091bea72e3f62682c3a6c3ab1fa48d6dd84d8c3
Alexey Dolgii <dolgii@devexperts.com>
2016-04-06 15:50:53 +0300

    tbd

--- src/main/thrift/tagservice.thrift
+++ src/main/thrift/tagservice.thrift
@@ -3,16 +3,20 @@ namespace java tagservice.service
 
 include "datamodel.thrift"
 
+exception TagServiceException {
+    1: string msg
+}
+
 service TagService {
-    void addTag(1: i64 recordId, 2: i64 tagId)
+    void addTag(1: i64 recordId, 2: i64 tagId) throws(1: TagServiceException ex)
 
-    void deleteTag(1: i64 recordId, 2: i64 tagId)
+    void deleteTag(1: i64 recordId, 2: i64 tagId) throws(1: TagServiceException ex)
 
-    list<datamodel.Tag> getTags(1: i64 recordId)
+    list<datamodel.Tag> getTags(1: i64 recordId) throws(1: TagServiceException ex)
 
-    list<datamodel.Record> getRecords(1: list<i64> tagIds)
+    list<datamodel.Record> getRecords(1: list<i64> tagIds) throws(1: TagServiceException ex)
 
     i64 createRecord(1: datamodel.Record record)
 
-    i64 createTag(1: datamodel.Tag tag)
+    i64 createTag(1: datamodel.Tag tag) throws(1: TagServiceException ex)
 }
